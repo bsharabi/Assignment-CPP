88764c410ead9b88090e6e2772b620ca0f77c63d
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-03-27 23:33:26 +0300

    new assingment-b

--- sources/Notebook.cpp
+++ sources/Notebook.cpp
@@ -8,45 +8,59 @@ typedef unsigned int uint;
 
 void Notebook::write(int page, int row_number, int column, ariel::Direction direction, string const &str)
 {
+    // Input integrity check
     if (!Notebook::validation(page, row_number, column, direction, 0, str))
     {
-        return;
+        throw invalid_argument("did not pass validation");
     }
+
     uint row = (uint)row_number;
     uint col = (uint)column;
     uint page_number = (uint)page;
     uint len = (uint)str.length();
     string line;
+
     // Checking the writing direction
     if (direction == Direction::Horizontal)
     {
         Notebook::createMultiLine(page_number, row);
+        // Gets the line
         line = notebook[page_number][row];
-        for (uint i = (uint)col, cnt = 0; i < len + (uint)col; i++, cnt++)
+        // A loop responsible for writing horizontally
+        for (uint i = col, cnt = 0; i < len + col; i++, cnt++)
         {
-            if (line[i] != '_' || str[cnt]=='~' || str[cnt] == '_')
+            // Input integrity check
+            if (line[i] != '_' || str[cnt] == '\n' || str[cnt] < ' ' || str[cnt] >= '~')
             {
-                throw invalid_argument("Can not write in this column");
+                throw invalid_argument("Can not write in this column Hor");
             }
 
             line[i] = str[cnt];
         }
+        // insert line to notebook
         notebook[page_number][row] = line;
     }
+
     else
     {
         Notebook::createMultiLine(page_number, row, len + 1);
-
-        for (uint i = (uint)row; direction == Direction::Vertical && i < (uint)row + len; i++)
+        bool flag = true;
+        // A loop responsible for writing horizontally
+        for (uint i = row, cnt = 0; i < row + len; i++, cnt++)
         {
-            line = Notebook::notebook[page_number][(uint)i];
-            if (line[col] != '_')
+            // Gets the line
+            line = Notebook::notebook[page_number][i];
+            // Input integrity check
+            if (line[col] != '_' || str[cnt] == '\n' || str[cnt] < ' ' || str[cnt] >= '~')
             {
+
+                throw invalid_argument("Can not write in this column ver");
+                flag = false;
                 return;
             }
-            // throw invalid_argument("Can not write in this column ver");
         }
-        for (uint i = (uint)row, cnt = 0; direction == Direction::Vertical && i < row + len; i++, cnt++)
+        // insert lines to notebook
+        for (uint i = row, cnt = 0; flag && i < row + len; i++, cnt++)
         {
             Notebook::notebook[page_number][i][col] = str[cnt];
         }
@@ -55,6 +69,7 @@ void Notebook::write(int page, int row_number, int column, ariel::Direction dire
 
 string Notebook::read(int page, int row_number, int column, ariel::Direction direction, int len)
 {
+    // Input integrity check
     if (!Notebook::validation(page, row_number, column, direction, 1, "f", len))
     {
         throw invalid_argument("did not pass validation");
@@ -66,15 +81,19 @@ string Notebook::read(int page, int row_number, int column, ariel::Direction dir
     uint length = (uint)len;
     string line;
     string word;
+    // Number of rows to create if not available
     uint count_row = (direction == Direction::Vertical) ? length + 1 : 3;
+
     Notebook::createMultiLine(page_number, row, count_row);
+    // A loop responsible for reading vertically
     for (uint i = row; direction == Direction::Vertical && i < row + length; i++)
     {
         line = Notebook::notebook[page_number][i];
         word += line[col];
     }
-
+    // Gets the line
     line = Notebook::notebook[page_number][row];
+    // A loop responsible for reading horizontally
     for (uint i = col; direction == Direction::Horizontal && i < col + length; i++)
     {
         word += line[i];
@@ -84,7 +103,8 @@ string Notebook::read(int page, int row_number, int column, ariel::Direction dir
 
 void Notebook::erase(int page, int row_number, int column, ariel::Direction direction, int len)
 {
-    if (!Notebook::validation(page, row_number, column, direction, 0, "ok", len))
+    // Input integrity check
+    if (!Notebook::validation(page, row_number, column, direction, 2, "ok", len))
     {
         return;
     }
@@ -95,13 +115,15 @@ void Notebook::erase(int page, int row_number, int column, ariel::Direction dire
     uint length = (uint)len;
     string line;
     string word;
+    // Number of rows to create if not available
     uint count_row = (direction == Direction::Vertical) ? length + 1 : 3;
     Notebook::createMultiLine(page_number, row, count_row);
+    // A loop responsible for deleting vertically
     for (uint i = row; direction == Direction::Vertical && i < row + length; i++)
     {
         Notebook::notebook[page_number][i][col] = '~';
     }
-
+    // A loop responsible for deleting horizontally
     for (uint i = col; direction == Direction::Horizontal && i < col + length; i++)
     {
         Notebook::notebook[page_number][row][i] = '~';
@@ -110,15 +132,22 @@ void Notebook::erase(int page, int row_number, int column, ariel::Direction dire
 
 void Notebook::show(int page_n)
 {
+    // Check the page number that is not negative
+    if (page_n < 0)
+    {
+        throw invalid_argument("Incorrect parameters");
+    }
+
     uint page_number = (uint)page_n;
+
+    // Create a page that has not yet been written and that we would like to show
     if (Notebook::pageExist(page_number))
     {
-        cout << "Page " + to_string(page_number) + " Does not exist" << endl;
-        return;
+        Notebook::setPage(page_number);
     }
 
     map<uint, string> page = Notebook::getPage(page_number);
-
+    // Print the page conveniently
     cout << "                                        -Page number(" << to_string(page_number) << ")-" << endl;
     for (auto const &kv : page)
     {
